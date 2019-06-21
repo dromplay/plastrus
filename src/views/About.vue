a6db9d3dbab95587a7e4eca2a4d20f7292a08535
lavlliet <riuzaki65@yandex.ru>
2019-06-21 17:36:25 +0300

    NEW NEW

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        {{ message }}
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue'
+
+    var app = new Vue({
+        el: '#app',
+        data: {
+            message: 'Привет, Vue!'
+        }
+    })
+</script>

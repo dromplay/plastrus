d31449a6d696c473c5d0cca6fc5db8dde10a97e0
lavlliet <riuzaki65@yandex.ru>
2019-06-21 17:32:20 +0300

    New

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
+
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        1
+        <div data-autoslide="0" class=" hide_line  section section149 mode_2  " style="background-color:#fcf9ed">
+            <div class="section_inner">
+                <div class="title  ">Материал теплиц<div><br></div></div>
+                <div class="sub_title">Теплицы на любой вкус</div>
+
+                <div style="width:1030px; margin:100px auto" class="arr1 ">
+
+                    <div style="float:left" class="col_3">
+                        <div class="image1  border   shadow  "><img style=" border: 10px solid #fcf9ed;" data-lazy="1" alt="" src="http://38806.lp.tobiz.net/img/300x225/6505ddf001bde3e56c51b46424c39d0b.jpg"></div>
+                        <div class="overlay_image_box ">
+                            <div class="overlay_image_title title1"><p  style="text-align: center;">Метал</p></div>
+                        </div>
+                    </div>
+
+                    <div  style="float:left;margin-left:35px" class="col_3">
+                        <div class="image2  border   shadow  "><img style=" border: 10px solid #fcf9ed;" data-lazy="1" alt="" src="http://38806.lp.tobiz.net/img/300x225/9bd02c335517a7c9d5dcb6fe782f6332.jpg"></div>
+                        <div class="overlay_image_box ">
+                            <div class="overlay_image_title title2"><p style="text-align: center;">Дерево</p></div>
+                        </div>2
+                    </div>
+
+                    <div style="float:left;margin-left:35px" class="col_3 last">
+                        <div class="image3  border   shadow  "><img style=" border: 10px solid #fcf9ed;" data-lazy="1" alt="" src="http://38806.lp.tobiz.net/img/300x225/47d24b3d13a8a815ef7aeb724f2ae124.jpg"></div>
+                        <div class="overlay_image_box ">
+                            <div class="overlay_image_title title3"><p style="text-align: center;">Поликарбонат</p></div>
+                        </div>
+                    </div>
+
+                    <div class="clear"></div>
+                  </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'home'
+    };
+
 </script>
+
+<style scoped>
+
+</style>
